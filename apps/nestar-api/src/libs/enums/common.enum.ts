993c6927508085a31f9f1f34e00236b697bd56e0
otash16 <otajonqutbiddinov1@gmail.com>
2024-08-20 15:29:18 +0500

    feat: build project standards

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,13 @@
+export enum Message{
+  SOMETHING_WENT_WRONG = "Something went wrong",
+  NO_DATA_FOUND = "No data is found",
+  CREATED_FAILED = "Create is failed",
+  UPDATE_FAILED = "Update is failed",
+
+  USED_NICK_PHONE = "you are inserting already used nick or phone!",
+  NO_MEMBER_NICK = "No member with that member nick!",
+  BLOCKED_USER = "You have been blocked, contact restaurant!",
+  WRONG_PASSWORD = "Wrong password, please try again!",
+  NOT_AUTHENTICATED = "You are not Authoticates, login first!",
+  TOKEN_CREATION_FAILED = "Token creation error",
+}

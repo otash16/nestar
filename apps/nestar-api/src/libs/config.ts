7ee754ecb4bd2562b369cc55e8735bcdee676c3d
otash16 <otajonqutbiddinov1@gmail.com>
2024-09-03 15:28:40 +0500

    feat: develop createProperty graphql api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -3,6 +3,16 @@ import { ObjectId } from 'bson';
 export const availableAgentSorts = ['createdAt', 'updtaedAt', 'memberLikes', 'memberViews', 'memberRank'];
 export const availableMemberSorts = ['createdAt', 'updtaedAt', 'memberLikes', 'memberViews'];
 
+/** IMAGE CONFIGURATION  */
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
+
 export const shapeIntoMogoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };

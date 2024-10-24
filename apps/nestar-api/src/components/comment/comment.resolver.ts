9b1f0e2ca4ce65f6c7f6ec2af2dc8d23b2cc8af1
otash16 <otajonqutbiddinov1@gmail.com>
2024-10-24 15:26:38 +0500

    feat: error fixed

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -54,7 +54,7 @@ export class CommentResolver {
 	@UseGuards(RolesGuard)
 	@Mutation((returns) => Comment)
 	public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment> {
-		console.log('Mutation: removeCommentByAdmin');
+		console.log('Mutattion: removeCommentByAdmin');
 		const commentId = shapeIntoMogoObjectId(input);
 		return await this.commentService.removeCommentByAdmin(commentId);
 	}
